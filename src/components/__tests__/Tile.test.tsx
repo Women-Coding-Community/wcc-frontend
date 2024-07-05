90ae67611a7fd6b96752714ba4aee8d8c89254fb
joana <joanabrito216@gmail.com>
2024-07-05 22:15:58 +0100

    Delete comment about icon testing section

--- src/components/__tests__/Tile.test.tsx
+++ src/components/__tests__/Tile.test.tsx
@@ -24,11 +24,8 @@ describe('Tile', () => {
   it('has the correct icon', () => {
     const icon = screen.getByText('diversity_2');
     expect(icon).toHaveClass('material-symbols-outlined');
-
-    // Ideally test to see if the font is applied, rather than class name
-    // The font family is not applied in this test because the Google Fonts stylesheet is not loaded
-    // expect(icon).toHaveStyle('font-family: "Material Symbol Outlined"');
   });
+
   it('has the correct name', () => {
     const name = screen.getByText('Section');
     expect(name).toBeInTheDocument();
