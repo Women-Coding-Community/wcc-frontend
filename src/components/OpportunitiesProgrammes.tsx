a0f1b92df7d7c9ae6a295f1f6910e4880f9c931f
joana <joanabrito216@gmail.com>
2024-07-05 17:24:49 +0100

    Update styling for mobile and destop view.

--- src/components/OpportunitiesProgrammes.tsx
+++ src/components/OpportunitiesProgrammes.tsx
@@ -12,13 +12,21 @@ export const OpportunitiesProgrammes = ({
 }) => {
   return (
     <GradientBox colors={['#FFB59D', '#FFDEA6']}>
-      <Box sx={{ display: 'grid', gap: '1rem', justifyItems: 'center' }}>
+      <Box
+        sx={{
+          display: 'grid',
+          gap: '24px',
+          justifyItems: 'center',
+          padding: { md: '0 80px' },
+        }}
+      >
         <Typography
           variant="h3"
           color="text.primary"
           align="center"
           sx={{
-            fontWeight: 'bold',
+            fontWeight: { xs: 500, md: 'bold' },
+            fontSize: { xs: '28px', sm: '28px' },
           }}
         >
           {content.title}
@@ -27,17 +35,20 @@ export const OpportunitiesProgrammes = ({
           variant="h5"
           color="text.primary"
           align="center"
-          sx={{ maxWidth: '600px' }}
+          sx={{
+            fontSize: { xs: '16px', sm: '16px', md: '24px' },
+            maxWidth: { xs: '361px', sm: '361px', md: '742px' },
+          }}
         >
           {content.description}
         </Typography>
         <Grid
-          style={{
+          container
+          spacing={{ xs: 3, sm: 3, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+          sx={{
             justifyContent: 'center',
           }}
-          container
-          spacing={{ xs: 2, sm: 2, md: 3 }}
-          columns={{ xs: 4, sm: 8, md: 12 }}
         >
           {content.programmes.map((p) => (
             <Grid item xs={12} sm={6} md={4} key={p.name}>
