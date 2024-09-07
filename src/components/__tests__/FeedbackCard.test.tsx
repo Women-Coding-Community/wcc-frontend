4147beccbac79591abb6adfca2ba649e85608ff6
joana <joanabrito216@gmail.com>
2024-09-07 12:01:15 +0100

    add test for feedback content

--- src/components/__tests__/FeedbackCard.test.tsx
+++ src/components/__tests__/FeedbackCard.test.tsx
@@ -3,9 +3,6 @@ import { render, screen } from '@testing-library/react';
 import { FeedbackCard } from 'components/FeedbackCard';
 
 describe('FeedbackCard', () => {
-  // const feedbackProps = {
-
-  // }
   beforeEach(() => {
     render(<FeedbackCard name={''} feedback={''} mentee={false} year={0} />);
   });
@@ -16,6 +13,21 @@ describe('FeedbackCard', () => {
   });
 
   it('has a feedback content', () => {
-    // const feedbackText = screen.getByText({feedback})
+    const feedbackText = 'This was a great experience!';
+    render(
+      <FeedbackCard
+        name="Jane"
+        feedback={feedbackText}
+        mentee={false}
+        year={2020}
+      />,
+    );
+    expect(screen.getByText(feedbackText)).toBeInTheDocument();
   });
+
+  it('has a name', () => {});
+
+  it('is mentee or mentor', () => {});
+
+  it('has the year when it was created', () => {});
 });
