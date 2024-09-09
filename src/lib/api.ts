23c7a66c88946f3c8e2f366e7bb63e95e15f490d
joana <joanabrito216@gmail.com>
2024-09-09 11:26:39 +0100

    fetch mentorship programme

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -10,11 +10,16 @@ export const fetchData = async (path: string) => {
   try {
     const response = await axios.get(`${apiBaseUrl}/${path}`);
     const footerData = await fetchFooter();
+    const mentorshipProgrammeData = await fetchMentorship();
 
     if (response.status !== 200) {
       throw new Error('Failed to fetch data');
     }
-    return { data: response.data, footer: footerData };
+    return {
+      data: response.data,
+      footer: footerData,
+      mentorship: mentorshipProgrammeData,
+    };
   } catch (error) {
     throw new Error('Failed to fetch data');
   }
@@ -32,5 +37,19 @@ export const fetchFooter = async () => {
     return response.data;
   } catch (error) {
     throw new Error('Failed to fetch footer');
+  }
+};
+const fetchMentorship = async () => {
+  try {
+    const response = await axios.get(
+      'https://wcc-backend.fly.dev/api/cms/v1/mentorship/overview',
+    );
+
+    if (response.status !== 200) {
+      throw new Error('Failed to fetch mentorship data');
+    }
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch mentorship');
   }
 };
