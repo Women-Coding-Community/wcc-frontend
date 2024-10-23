85545842af26c33764e247d93eba95a252825f8b
joana <joanabrito216@gmail.com>
2024-10-23 15:54:38 +0100

    Created a new component and added some logic

---
+++ src/pages/mentorship/feedback-section.tsx
@@ -0,0 +1,28 @@
+// path: /mentorship/faqs
+
+// import { Button } from "@mui/material";
+
+// import { Feedback } from '@mui/icons-material';
+// import { Typography } from '@mui/material';
+// import FeedbackCard from 'componets'
+
+const FeedbackSection = () => {
+  //   const { title, feedbacks } = feedbackSecion;
+  return (
+    <div></div>
+    // <div>
+    //   <Typography variant="h4">Welcome to the Feedback section</Typography>
+    //   <h2>{title}</h2>
+    //   <div>
+    //     {feedbacks.length > 0 ? (
+    //       feedbacks.map((feedback, index) => <FeedbackCard />)
+    //     ) : (
+    //       <p></p>
+    //     )}
+    //   </div>
+    // <Button></Button>
+    // </div>
+  );
+};
+
+export default FeedbackSection;
