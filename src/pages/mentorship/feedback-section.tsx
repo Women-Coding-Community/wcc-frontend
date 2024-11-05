8ffea3b3dde6d1aec82a2bb0c8506bfddce787f1
joana <joanabrito216@gmail.com>
2024-11-05 22:21:20 +0000

    Created the feedback Section

--- src/pages/mentorship/feedback-section.tsx
+++ src/pages/mentorship/feedback-section.tsx
@@ -1,28 +1,80 @@
 // path: /mentorship/faqs
 
-// import { Button } from "@mui/material";
-
-// import { Feedback } from '@mui/icons-material';
-// import { Typography } from '@mui/material';
 // import FeedbackCard from 'componets'
 
-const FeedbackSection = () => {
-  //   const { title, feedbacks } = feedbackSecion;
+import { Box, Typography, Button } from '@mui/material';
+import React from 'react';
+
+// import QuoteIcon from '../../../public/logo_white.png';
+
+interface FeedbackCardProps {
+  name: string;
+  feedback: string;
+  mentee: boolean;
+  year: number;
+}
+
+export const FeedbackCard: React.FC<FeedbackCardProps> = ({
+  name,
+  feedback,
+  mentee,
+  year,
+}) => {
+  const boldText = `${name}, ${mentee ? 'Mentee' : 'Mentor'} ${year}`;
+
   return (
-    <div></div>
-    // <div>
-    //   <Typography variant="h4">Welcome to the Feedback section</Typography>
-    //   <h2>{title}</h2>
-    //   <div>
-    //     {feedbacks.length > 0 ? (
-    //       feedbacks.map((feedback, index) => <FeedbackCard />)
-    //     ) : (
-    //       <p></p>
-    //     )}
-    //   </div>
-    // <Button></Button>
-    // </div>
+    <Box
+      sx={{
+        display: 'grid',
+        gap: '1rem',
+        borderRadius: '12px',
+        backgroundColor: 'white',
+        padding: '2rem',
+        maxWidth: '400px',
+        margin: '0 auto',
+      }}
+    >
+      <Typography variant="body2">{feedback}</Typography>
+      <Typography
+        variant="subtitle2"
+        sx={{ fontWeight: '500', lineHeight: '20px', letterSpacing: '0.1px' }}
+      >
+        {boldText}
+      </Typography>
+    </Box>
   );
 };
 
-export default FeedbackSection;
+interface FeedbackSectionProps {
+  title: string;
+  feedbacks: FeedbackCardProps[];
+}
+
+export const FeedbackSection: React.FC<FeedbackSectionProps> = ({
+  title,
+  feedbacks,
+}) => {
+  return (
+    <>
+      <div>
+        <Typography variant="h3">{title}</Typography>
+        <div>
+          {feedbacks && feedbacks.length > 0 ? (
+            feedbacks.map((feedback, index) => (
+              <FeedbackCard
+                key={index}
+                name={feedback.name}
+                feedback={feedback.feedback}
+                mentee={feedback.mentee}
+                year={feedback.year}
+              />
+            ))
+          ) : (
+            <p></p>
+          )}
+        </div>
+        <Button>+ Show more</Button>
+      </div>
+    </>
+  );
+};
