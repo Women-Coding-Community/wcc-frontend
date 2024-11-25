fd5d98bc69372c753996194066f12eddb4df9aa0
joana <joanabrito216@gmail.com>
2024-11-25 17:14:29 +0000

    Fixed sonar found issues

--- src/pages/mentorship/feedback-section.tsx
+++ src/pages/mentorship/feedback-section.tsx
@@ -22,73 +22,70 @@ export const FeedbackSection: React.FC<FeedbackSectionProps> = ({
     );
   };
   return (
-    <>
-      <ColoredBox color={'#FFDEA6'}>
-        <Box sx={{ display: 'grid', justifyItems: 'center', gap: '3rem' }}>
-          <Typography
-            variant="h3"
-            sx={{
-              fontSize: { xs: '24px', sm: '24px', md: '45px' },
-              maxWidth: { xs: '361px', sm: '361px', md: '742px' },
-              lineHeight: { xs: '32px', sm: '32px', md: '52px' },
-              fontWeight: 600,
-              paddingTop: '2rem',
-              textAlign: 'center',
-            }}
-          >
-            {title}
-          </Typography>
-          <Grid
-            container
-            alignItems="stretch"
-            spacing={{ xs: 3, sm: 3, md: 3 }}
-            columns={{ xs: 4, sm: 8, md: 12 }}
-            sx={{
-              justifyContent: 'flex-start',
-              maxWidth: '70rem',
-            }}
-          >
-            {feedbacks && feedbacks.length > 0 ? (
-              feedbacks.slice(0, feedbacksDisplayed).map((feedback, index) => (
-                <Grid
-                  item
-                  xs={12}
-                  sm={6}
-                  md={4}
-                  key={feedback.name}
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'stretch',
-                  }}
-                >
-                  <FeedbackCard
-                    key={index}
-                    name={feedback.name}
-                    feedback={feedback.feedback}
-                    mentee={feedback.mentee}
-                    year={feedback.year}
-                  />
-                </Grid>
-              ))
-            ) : (
-              <p>There‵s no feedback yet!</p>
-            )}
-          </Grid>
+    <ColoredBox color={'#FFDEA6'}>
+      <Box sx={{ display: 'grid', justifyItems: 'center', gap: '3rem' }}>
+        <Typography
+          variant="h3"
+          sx={{
+            fontSize: { xs: '24px', sm: '24px', md: '45px' },
+            maxWidth: { xs: '361px', sm: '361px', md: '742px' },
+            lineHeight: { xs: '32px', sm: '32px', md: '52px' },
+            fontWeight: 600,
+            paddingTop: '2rem',
+            textAlign: 'center',
+          }}
+        >
+          {title}
+        </Typography>
+        <Grid
+          container
+          alignItems="stretch"
+          spacing={{ xs: 3, sm: 3, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+          sx={{
+            justifyContent: 'flex-start',
+            maxWidth: '70rem',
+          }}
+        >
+          {feedbacks && feedbacks.length > 0 ? (
+            feedbacks.slice(0, feedbacksDisplayed).map((feedback) => (
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={4}
+                key={feedback.name}
+                sx={{
+                  display: 'flex',
+                  alignItems: 'stretch',
+                }}
+              >
+                <FeedbackCard
+                  name={feedback.name}
+                  feedback={feedback.feedback}
+                  mentee={feedback.mentee}
+                  year={feedback.year}
+                />
+              </Grid>
+            ))
+          ) : (
+            <p>There‵s no feedback yet!</p>
+          )}
+        </Grid>
 
-          <Button
-            onClick={showMoreFeedbacks}
-            disabled={feedbacksDisplayed >= feedbacks.length}
-            variant="outlined"
-            sx={{
-              borderRadius: '20px',
-              border: '1px solid #71787E',
-              color: '#1A4B66',
-            }}
-          >
-            + Show more
-          </Button>
-        </Box>
-      </ColoredBox>
-    </>
+        <Button
+          onClick={showMoreFeedbacks}
+          disabled={feedbacksDisplayed >= feedbacks.length}
+          variant="outlined"
+          sx={{
+            borderRadius: '20px',
+            border: '1px solid #71787E',
+            color: '#1A4B66',
+          }}
+        >
+          + Show more
+        </Button>
+      </Box>
+    </ColoredBox>
   );
 };
