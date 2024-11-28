2011e93b28cf54d33f10e2f58e5f324e85b4eece
menaesezobor <menaesezobor@gmail.com>
2024-11-28 01:37:53 +0000

    feat: Add section as component

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,10 +1,39 @@
+import { Box, Typography, useMediaQuery } from '@mui/material';
+
+import theme from 'theme';
+
 type TeamSectionProps = {
-  page: {
-    title: string;
-    subtitle: string;
-    description: string;
-  };
+  title: string;
+  subtitle: string;
+  description: string;
 };
-export const Section = ({ page }: TeamSectionProps) => {
-  return <div>{page.description}</div>;
+export const Section = (props: TeamSectionProps) => {
+  const isMobile = useMediaQuery(theme.breakpoints.down(750));
+  return (
+    <Box textAlign={'left'} marginRight={isMobile ? '' : '52px'}>
+      <Typography
+        variant="h3"
+        color="text.primary"
+        align="left"
+        sx={{
+          fontWeight: { xs: 600, md: 'bold' },
+          fontSize: { xs: '45px', sm: '45px' },
+        }}
+      >
+        {props.subtitle}
+      </Typography>
+      <Typography
+        variant="h6"
+        color={theme.palette.primary.dark}
+        sx={{
+          marginY: 2,
+          fontSize: '1rem',
+          fontWeight: '500',
+          lineHeight: '1.2',
+        }}
+      >
+        {props.description}
+      </Typography>
+    </Box>
+  );
 };
