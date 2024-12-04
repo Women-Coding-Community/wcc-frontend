d1252f761451bbd550a8f59387a15f4f93c3afbf
Joana Brito <140211258+joanaBrit@users.noreply.github.com>
2024-12-04 15:31:26 +0000

    Feat 27 feedback section (#52)
    * Created a new component and added some logic
    
    * Changed the type MentorshipProgrammeData feedback to feedbackSection
    
    * Created the feedback Section
    
    * displayed the feedback Section
    
    * Created a component to use for solid color on the background
    
    * Added the component to index exports
    
    * exported the interface FeedbackCardProps
    
    * Eliminated unnecessary code and added styling.
    
    * Added height 100% to the card, to keep it all of these the same size
    
    * Create a grid and styled other components
    
    * Testing styling
    
    * Limited the showing cards to three and styling
    
    * Deleted a comment
    
    * Fixed sonar found issues
    
    * Styled the cards to have all the same height on laptop view
    
    * Moved feedback section into index.tsx instead of separate page path

--- src/pages/mentorship/index.tsx
+++ src/pages/mentorship/index.tsx
@@ -1,6 +1,10 @@
+import { Typography, Button, Box } from '@mui/material';
 import { GetServerSideProps } from 'next';
+import React, { useState } from 'react';
 
+import { ColoredBox, FeedbackCard } from '@components';
 import { MentorshipProgrammeData } from '@utils/types';
+import { FeedbackCardProps } from 'components/FeedbackCard';
 import { fetchData } from 'lib/api';
 
 interface MentorshipPageProps {
@@ -8,17 +12,100 @@ interface MentorshipPageProps {
   error: string | null;
 }
 
+interface FeedbackSectionProps {
+  title: string;
+  feedbacks: FeedbackCardProps[];
+}
+
 const MentorshipPage = ({ mentorship }: MentorshipPageProps) => {
-  return <div>{JSON.stringify(mentorship)}</div>;
+  return (
+    <div>
+      <FeedbackSection
+        title={mentorship.feedbackSection.title}
+        feedbacks={mentorship.feedbackSection.feedbacks}
+      />
+    </div>
+  );
+};
+
+const FeedbackSection: React.FC<FeedbackSectionProps> = ({
+  title,
+  feedbacks,
+}) => {
+  const [feedbacksDisplayed, setFeedbacksDisplayed] = useState<number>(3);
+  const showMoreFeedbacks = () => {
+    setFeedbacksDisplayed((prevCount) =>
+      Math.min(prevCount + 3, feedbacks.length),
+    );
+  };
+  return (
+    <ColoredBox color={'#FFDEA6'}>
+      <Box sx={{ display: 'grid', justifyItems: 'center', gap: '3rem' }}>
+        <Typography
+          variant="h3"
+          sx={{
+            fontSize: { xs: '24px', sm: '24px', md: '45px' },
+            maxWidth: { xs: '361px', sm: '361px', md: '742px' },
+            lineHeight: { xs: '32px', sm: '32px', md: '52px' },
+            fontWeight: 600,
+            paddingTop: '2rem',
+            textAlign: 'center',
+          }}
+        >
+          {title}
+        </Typography>
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: { sm: 'repeat(3, 1fr)', md: '' },
+            gap: 2,
+            gridTemplateRows: {
+              sm: feedbacksDisplayed > 3 ? '1fr 1fr' : '',
+              md: '',
+            },
+          }}
+        >
+          {feedbacks && feedbacks.length > 0 ? (
+            feedbacks
+              .slice(0, feedbacksDisplayed)
+              .map((feedback) => (
+                <FeedbackCard
+                  key={feedback.name}
+                  name={feedback.name}
+                  feedback={feedback.feedback}
+                  mentee={feedback.mentee}
+                  year={feedback.year}
+                />
+              ))
+          ) : (
+            <p>There‵s no feedback yet!</p>
+          )}
+        </Box>
+
+        <Button
+          onClick={showMoreFeedbacks}
+          disabled={feedbacksDisplayed >= feedbacks.length}
+          variant="outlined"
+          sx={{
+            borderRadius: '20px',
+            border: '1px solid #71787E',
+            color: '#1A4B66',
+          }}
+        >
+          + Show more
+        </Button>
+      </Box>
+    </ColoredBox>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
   try {
-    const combinedResponse = await fetchData('mentorship/overview');
+    const response = await fetchData('mentorship/overview');
 
     return {
       props: {
-        mentorship: combinedResponse.data,
+        mentorship: response.data,
       },
     };
   } catch (error) {
