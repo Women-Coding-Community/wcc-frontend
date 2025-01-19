727580a0b02e17208931ede9fafdb362d2fa046a
Idayat Sanni <idayatsanni@gmail.com>
2025-01-19 14:22:10 -0500

    update hero image path

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,6 @@ interface HeroProps {
 
 export const Hero: React.FC<HeroProps> = ({ title, description, images }) => {
   const { alt: imageAlt, path: imagePath } = images[0];
-  // currently the `/` is missing from the path value
   const tempImagePathForApplication = `${imagePath}`;
   const isMobile = useMediaQuery(theme.breakpoints.down(750));
 
