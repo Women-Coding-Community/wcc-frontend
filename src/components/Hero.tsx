c15147be6ee6551a605d80fc689828b67ea83daf
Steph <stepsen.dev@gmail.com>
2025-01-19 14:50:32 +0000

    testing vercel connection

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,8 +15,8 @@ interface HeroProps {
 
 export const Hero: React.FC<HeroProps> = ({ title, description, images }) => {
   const { alt: imageAlt, path: imagePath } = images[0];
+  console.log(imagePath);
   // currently the `/` is missing from the path value
-  const tempImagePathForApplication = `/${imagePath}`;
   const isMobile = useMediaQuery(theme.breakpoints.down(750));
 
   return (
@@ -69,7 +69,7 @@ export const Hero: React.FC<HeroProps> = ({ title, description, images }) => {
         <Grid item xs={12} sm={7} style={{ padding: 0, margin: 0 }}>
           <Box>
             <Image
-              src={tempImagePathForApplication}
+              src={imagePath}
               alt={imageAlt}
               width={647}
               height={374}
