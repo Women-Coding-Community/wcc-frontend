cbe1f415f9307c1d6aeb3b2f84b969087afa6dcc
joana <joanabrito216@gmail.com>
2025-02-16 22:19:33 +0000

    create fakeData to test my component

--- src/pages/about-us/team/index.tsx
+++ src/pages/about-us/team/index.tsx
@@ -2,10 +2,20 @@
 
 import { Typography } from '@mui/material';
 
+import { ContactBox } from 'components/ContactBox';
+
+const fakeData = {
+  title: 'Contact US',
+  links: [
+    { linkText: 'website', path: 'https://mui.com/material-ui/', icon: '🙌' },
+  ],
+};
+
 const TeamLeadershipPage = () => {
   return (
     <div>
       <Typography variant="h4">Welcome to the TeamPage</Typography>
+      <ContactBox title={fakeData.title} links={fakeData.links}></ContactBox>
     </div>
   );
 };
