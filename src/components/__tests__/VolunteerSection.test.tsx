069c9295de6c274ea90f19952b9e4a8d49380d92
Idayat Sanni <idayatsanni@gmail.com>
2025-02-17 22:01:52 -0500

    remove cleanup

--- src/components/__tests__/VolunteerSection.test.tsx
+++ src/components/__tests__/VolunteerSection.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 import { VolunteerSection } from '../VolunteerSection';
