7dbf2391603454b88357be6cd387fe21ec45f999
Idayat Sanni <idayatsanni@gmail.com>
2025-02-17 20:16:36 -0500

    change fallback for images

--- src/components/MentorBanner.tsx
+++ src/components/MentorBanner.tsx
@@ -22,7 +22,7 @@ export const MentorBanner: React.FC<BannerProps> = ({
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   const { alt: desktopAlt, path: desktopPath } = images[0];
-  const { alt: mobileAlt, path: mobilePath } = images[1] || {};
+  const { alt: mobileAlt, path: mobilePath } = images[1] || images[0];
 
   const { uri: linkUri, label: linkLabel } = link;
 
