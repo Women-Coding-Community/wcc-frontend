10304b4ede4b8d315cc6ec08298bf529a887782a
joana <joanabrito216@gmail.com>
2025-03-16 00:18:46 +0000

    Style for Icon and titlelink

--- src/components/ContactBox.tsx
+++ src/components/ContactBox.tsx
@@ -1,40 +1,78 @@
 import { Box, Typography } from '@mui/material';
 import Link from 'next/link';
-import React from 'react';
+import React, { CSSProperties } from 'react';
+
+import SlackIcon from '@public/icons/slack-icon.svg';
 
 interface ContactBoxProps {
-  // titleLink is the href for the title
   title: string;
   titleLink?: string;
-  titleIcon?: React.ReactNode;
+  // titleIcon?: React.ReactNode;
   text?: string;
   children?: React.ReactNode;
-  // links are for links to  email, slack etc
   links?: Array<{ linkText: string; path: string; icon: React.ReactNode }>;
+  titleStyle?: CSSProperties;
+  // boxStylePosition: CSSProperties;
 }
 
 export const ContactBox: React.FC<ContactBoxProps> = ({
   title,
   titleLink,
-  titleIcon,
+  // titleIcon,
   text,
   children,
   links,
+  titleStyle,
+  // boxStylePosition,
 }) => {
   return (
     <Box
-      sx={{ width: 150, height: 100, bgcolor: '#F6FAFE', borderRadius: '12px' }}
+      sx={{
+        maxWidth: '358px',
+        // height: 304,
+        bgcolor: 'primary.light',
+        borderRadius: '12px',
+        textAlign: 'left',
+        padding: '24px',
+        // gap: '32px',
+        border: '2px solid red',
+        margin: '1px 15px',
+        // ...boxStylePosition,
+      }}
     >
-      {titleIcon}
-      <Typography variant="h5">
-        {titleLink ? <Link href={titleLink}>{title}</Link> : title}
+      <SlackIcon />
+      <Typography variant="h5" lineHeight={'36px'}>
+        {titleLink ? (
+          <Link
+            href={titleLink}
+            style={{
+              textDecoration: 'none',
+              color: 'primary.main', // Style not applying
+              ...titleStyle,
+            }}
+          >
+            {title}
+          </Link>
+        ) : (
+          title
+        )}
       </Typography>
       <Typography>{text}</Typography>
       {Array.isArray(links) &&
         links.map(({ linkText, path, icon }) => (
-          <Link href={path} key={path}>
-            {icon}
-            {linkText}
+          <Link href={path} key={path} style={{ textDecoration: 'none' }}>
+            <span style={{ fontSize: '24px', paddingRight: '3px' }}>
+              {icon}
+            </span>
+            <span
+              style={{
+                textDecoration: 'underline',
+                color: '#1A4B66',
+                paddingLeft: '3px',
+              }}
+            >
+              {linkText}
+            </span>
           </Link>
         ))}
       {children}
