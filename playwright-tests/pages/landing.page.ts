7ef486765528e0f184657982d0175ffc24a98b43
nora-weisser <eleo.belova@yandex.ru>
2025-06-13 20:29:29 +0200

    feat: cleaned up the landing page file

--- playwright-tests/pages/landing.page.ts
+++ playwright-tests/pages/landing.page.ts
@@ -6,9 +6,6 @@ export class LandingPage extends BasePage {
   readonly heroTitle: Locator;
   readonly heroSubtitle: Locator;
   readonly joinSlackButton: Locator;
-  readonly heroImage: Locator;
-  readonly opportunitiesTitle: Locator;
-  readonly opportunitiesDescription: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -19,14 +16,5 @@ export class LandingPage extends BasePage {
       name: 'Empowering Women in Their Tech Careers',
     });
     this.joinSlackButton = page.getByRole('link', { name: 'Join our Slack' });
-    this.heroImage = page
-      .getByTestId('hero-container')
-      .getByRole('img', { name: 'There are two women talking' });
-    this.opportunitiesTitle = page.getByRole('heading', {
-      name: 'Opportunities and Programmes',
-    });
-    this.opportunitiesDescription = page.getByText(
-      /Join our community and unlock endless opportunities/i,
-    );
   }
 }
