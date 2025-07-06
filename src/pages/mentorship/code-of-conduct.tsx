41dfd0d553bc99f07e5f622ff53ba89b873e6d65
Lizdigital <lizetteclaireh@gmail.com>
2025-07-06 20:15:06 +0100

    Add Mentorship Code of Conduct page header

--- src/pages/mentorship/code-of-conduct.tsx
+++ src/pages/mentorship/code-of-conduct.tsx
@@ -1,14 +1,27 @@
 // path: /mentorship/code-of-conduct
 
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 
 const MentorshipCodeOfConductPage = () => {
   return (
-    <div>
-      <Typography variant="h4">
-        Welcome to the MentorshipCodeOfConductPage
+    <Box
+      sx={{
+        background: 'linear-gradient(270deg, #9FCCEC 0%, #C7E7FF 100%)',
+        height: '160px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Typography
+        variant="h4"
+        sx={{
+          fontWeight: 600,
+        }}
+      >
+        Mentorship Code of Conduct
       </Typography>
-    </div>
+    </Box>
   );
 };
 
