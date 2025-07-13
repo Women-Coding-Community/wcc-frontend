c3a8a63e148fd365b46d2f7d6cf42d53d7e80405
nora-weisser <eleo.belova@yandex.ru>
2025-07-13 19:40:53 +0200

    feat: small fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,8 +13,7 @@ import {
   MenuItem,
   Typography,
   useMediaQuery,
-  useTheme,
-  Box,
+  useTheme
 } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
