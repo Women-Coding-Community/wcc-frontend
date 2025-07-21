4a5936029abe2be3a0e1afc496c2101194e5b8c2
nora-weisser <eleo.belova@yandex.ru>
2025-07-21 21:58:38 +0200

    feat: added navigation test cases

--- playwright-tests/tests/landing.page.spec.ts
+++ playwright-tests/tests/landing.page.spec.ts
@@ -1,12 +1,9 @@
+import { navTests } from '@utils/datafactory/nav.tests';
 import { expect } from '@playwright/test';
-
 import { test } from '@utils/fixtures';
 
-test.beforeEach(async ({ page }) => {
+test('should display complete navigation menu', async ({ page, landingPage }) => {
   await page.goto('/');
-});
-
-test('should display complete navigation menu', async ({ landingPage }) => {
   await expect(landingPage.homeLink).toBeVisible();
   await expect(landingPage.mentorshipDropdown).toBeVisible();
   await expect(landingPage.programmesDropdown).toBeVisible();
@@ -20,6 +17,7 @@ test('Join Slack button navigates to Slack invite in the new page', async ({
   page,
   landingPage,
 }) => {
+  await page.goto('/');
   const newPagePromise = page.waitForEvent('popup');
   await landingPage.joinSlackButton.click();
   const newPage = await newPagePromise;
@@ -27,4 +25,28 @@ test('Join Slack button navigates to Slack invite in the new page', async ({
   await newPage.waitForLoadState();
   expect(newPage.url()).toContain('slack.com');
   await expect(newPage).toHaveTitle(/Slack/i);
+});
+
+test.describe('Navigation Links', () => {
+  navTests.forEach(({ id, linkName: linkName, pathToStart, linkLocator: linkLocator, expectedURL, expectedText }) => {
+    test(`${id}: Test "${linkName}" Link Navigation`, async ({ page, landingPage }) => {
+      await test.step(`Navigate to starting path: ${pathToStart}`, async () => {
+        await page.goto(pathToStart);
+      });
+
+      await test.step(`Click the "${linkName}" link`, async () => {
+        await linkLocator(landingPage).click();
+      });
+
+      await test.step(`Verify URL is "${expectedURL}"`, async () => {
+        await expect(page).toHaveURL(expectedURL);
+      });
+
+      if (expectedText) {
+        await test.step(`Verify page contains text "${expectedText}"`, async () => {
+          await expect(page.getByText(expectedText, { exact: true })).toBeVisible();
+        });
+      }
+    });
+  });
 });
