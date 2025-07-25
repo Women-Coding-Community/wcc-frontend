908ef06055a470c0f50afd47452af81c2c503311
nora-weisser <eleo.belova@yandex.ru>
2025-07-25 14:42:20 +0200

    feat: added steps to the methods and parameterized test cases for navigation

--- playwright-tests/tests/landing.page.spec.ts
+++ playwright-tests/tests/landing.page.spec.ts
@@ -1,6 +1,4 @@
 import { expect } from '@playwright/test';
-
-import { navTests } from '@utils/datafactory/nav.tests';
 import { test } from '@utils/fixtures';
 
 test('should display complete navigation menu', async ({
@@ -29,42 +27,4 @@ test('Join Slack button navigates to Slack invite in the new page', async ({
   await newPage.waitForLoadState();
   expect(newPage.url()).toContain('slack.com');
   await expect(newPage).toHaveTitle(/Slack/i);
-});
-
-test.describe('Navigation Links', () => {
-  navTests.forEach(
-    ({
-      id,
-      linkName: linkName,
-      pathToStart,
-      linkLocator: linkLocator,
-      expectedURL,
-      expectedText,
-    }) => {
-      test(`${id}: Test "${linkName}" Link Navigation`, async ({
-        page,
-        landingPage,
-      }) => {
-        await test.step(`Navigate to starting path: ${pathToStart}`, async () => {
-          await page.goto(pathToStart);
-        });
-
-        await test.step(`Click the "${linkName}" link`, async () => {
-          await linkLocator(landingPage).click();
-        });
-
-        await test.step(`Verify URL is "${expectedURL}"`, async () => {
-          await expect(page).toHaveURL(expectedURL);
-        });
-
-        if (expectedText) {
-          await test.step(`Verify page contains text "${expectedText}"`, async () => {
-            await expect(
-              page.getByText(expectedText, { exact: true }),
-            ).toBeVisible();
-          });
-        }
-      });
-    },
-  );
 });
---
+++ playwright-tests/tests/navigation.page.spec.ts
@@ -0,0 +1,27 @@
+import { navTests } from '@utils/datafactory/nav.tests';
+import { test } from '@utils/fixtures';
+
+test.describe('Validate Navigation Menu', () => {
+    navTests.forEach(
+        ({
+            id,
+            linkName: linkName,
+            pathToStart,
+            linkLocator: linkLocator,
+            expectedURL,
+            expectedText,
+        }) => {
+            test.only(`${id}: Test "${linkName}" Link Navigation`, async ({
+                landingPage,
+                basePage
+            }) => {
+                await basePage.navigateToPath(pathToStart);
+                await basePage.clickElement(linkLocator(landingPage));
+                await basePage.verifyURL(expectedURL);
+                if (expectedText) {
+                    await basePage.verifyPageContainsText(expectedText);
+                }
+            });
+        },
+    );
+});
