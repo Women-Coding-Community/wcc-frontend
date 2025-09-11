f2104b71dc4861ae8f2531e95fb54673062a6e7b
AnushaDeviR <anusha3599@gmail.com>
2025-09-11 21:32:21 +0100

    issue-80: revert changes

--- src/pages/mentorship/long-term-timeline.tsx
+++ src/pages/mentorship/long-term-timeline.tsx
@@ -1,22 +1,14 @@
 // path: /mentorship/long-term-timeline
 
-import { Title, TimelineCard } from '@components';
+import { Typography } from '@mui/material';
 
 const MentorshipLongTermTimelinePage = () => {
   return (
-    <>
-      <Title title="Long-Term Mentorship Timeline" />
-      <TimelineCard
-        date="Month, YYYY - Month, YYYY"
-        title="Timeline Title"
-        description="Mentors can apply for Mentorship throughout the year. Once a profile has been approved and published on the website, the mentor will receive a confirmation email."
-      />
-      <TimelineCard
-        date="Month, YYYY - Month, YYYY"
-        title="Midpoint Check-In"
-        description="test test"
-      />
-    </>
+    <div>
+      <Typography variant="h4">
+        Welcome to the Long Term Timeline Page
+      </Typography>
+    </div>
   );
 };
 
