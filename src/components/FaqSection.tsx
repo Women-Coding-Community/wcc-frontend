3573ad2e9e06556d4ddeece49bf3d419264d287f
Mitali Shah <mitalishaleenshah@gmail.com>
2025-09-12 15:52:35 -0500

    feat: single single-open logic/ FAQ accordions

--- src/components/FaqSection.tsx
+++ src/components/FaqSection.tsx
@@ -6,24 +6,55 @@ import {
   Box,
   Typography,
 } from '@mui/material';
-import React from 'react';
+import React, { useState, SyntheticEvent } from 'react';
 
 import { FaqSectionProps } from '../utils/types';
 
 export const FaqSection: React.FC<FaqSectionProps> = ({ title, items }) => {
+  const [expanded, setExpanded] = useState<string | false>(false);
+
+  const handleChange =
+    (accordionId: string) => (event: SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? accordionId : false);
+    };
+
   return (
     <Box sx={{ mb: 4 }}>
       <Typography variant="h5">{title}</Typography>
-      {items.map((item) => (
-        <Accordion key={item.question}>
-          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>{item.question}</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>{item.answer}</Typography>
-          </AccordionDetails>
-        </Accordion>
-      ))}
+      {items.map((item, index) => {
+        const accordionId = `accordion-${index}`;
+
+        return (
+          <Accordion
+            key={accordionId}
+            expanded={expanded === accordionId}
+            onChange={handleChange(accordionId)}
+            sx={{
+              '&:before': {
+                display: 'none',
+              },
+              borderBottom: '1px solid',
+              borderColor: 'divider',
+              boxShadow: 'none',
+              '&:first-of-type': {
+                borderTop: '1px solid',
+                borderColor: 'divider',
+              },
+            }}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`${accordionId}-content`}
+              id={`${accordionId}-header`}
+            >
+              <Typography>{item.question}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>{item.answer}</Typography>
+            </AccordionDetails>
+          </Accordion>
+        );
+      })}
     </Box>
   );
 };
