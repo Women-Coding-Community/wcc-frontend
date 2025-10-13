fe89f6d6ab085db2d05f46d644674561790df0cf
PranitaVPanse <pranita.v.panse@gmail.com>
2025-10-13 11:32:00 +0100

    #136 Added playwright tests for Volunteer Section on Home Page

--- playwright-tests/pages/home.page.ts
+++ playwright-tests/pages/home.page.ts
@@ -3,22 +3,34 @@ import { Locator, Page } from '@playwright/test';
 import { BasePage } from '@pages/base.page';
 
 export class HomePage extends BasePage {
-  readonly becomeMentorSection: Locator;
-  readonly sectionTitle: Locator;
-  readonly sectionDescription: Locator;
+  readonly becomeMentorSectionTitle: Locator;
+  readonly becomeMentorSectionDescription: Locator;
   readonly joinAsMentorBtn: Locator;
+
+  readonly volunteerSectionTitle: Locator;
+  readonly volunteerSectionDescription: Locator;
+  readonly learnMoreVolunteerBtn: Locator;
 
   constructor(page: Page) {
     super(page);
-    this.becomeMentorSection = page.getByTestId('mentor-banner');
-    this.sectionTitle = page.getByRole('heading', {
+
+    // Become a Mentor section locators
+    this.becomeMentorSectionTitle = page.getByRole('heading', {
       name: 'Become a Mentor',
       exact: true,
     });
-    this.sectionDescription = page.getByRole('heading', {
+    this.becomeMentorSectionDescription = page.getByRole('heading', {
       name: 'Ready to empower and be empowered in tech? Become a mentor! Expand your network, give back, share expertise, and discover new perspectives.',
       exact: true,
     });
     this.joinAsMentorBtn = page.getByRole('link', { name: 'Join as a mentor' });
+
+    // Volunteer section locators
+    this.volunteerSectionTitle = page.getByRole('heading', {
+      name: 'Do you want to volunteer with us?',
+      exact: true,
+    });
+    this.volunteerSectionDescription = page.locator('p.MuiTypography-root.MuiTypography-body1.css-11xjirw-MuiTypography-root');
+    this.learnMoreVolunteerBtn = page.getByRole('link', { name: 'Learn more about volunteering' });
   }
 }
