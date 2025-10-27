b733bc37517e42a3bbe1f2e15308eeaeb3e5dd18
Sandra Barbosa <sandrabarbosa@Sandras-MacBook-Pro.local>
2025-10-27 21:40:28 +0000

    Create reusable Hero component and add footer to page

--- src/pages/mentorship/study-groups.tsx
+++ src/pages/mentorship/study-groups.tsx
@@ -1,13 +1,24 @@
 // path: /mentorship/study-groups
 
 import { Typography } from '@mui/material';
+import { StudyGroupsHero } from '../../components/StudyGroupHero';
+import { Footer } from '@components';
+import footerData from '../../lib/responses/footer.json';
 
 const MentorShipStudyGroupsPage = () => {
   return (
     <div>
-      <Typography variant="h4">
+      <StudyGroupsHero
+        title="Technical Study Groups"
+        imageSrc="/hero-img.jpg"
+      />
+      <Typography
+        variant="h4"
+        sx={{ padding: '20px 16px', textAlign: 'center' }}
+      >
         Welcome to the MentorShipStudyGroupsPage
       </Typography>
+      <Footer {...footerData} />
     </div>
   );
 };
