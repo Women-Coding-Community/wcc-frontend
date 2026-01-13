02942b04535ac4a3fbcdd2dc2e7dd2adc74064bb
AdniSosa <adni.sori@gmail.com>
2026-01-13 12:00:44 +0100

    Added timeline lines and dots, footer, and reviewed the design

--- src/pages/mentorship/long-term-timeline.tsx
+++ src/pages/mentorship/long-term-timeline.tsx
@@ -1,8 +1,14 @@
 // path: /mentorship/long-term-timeline
 
+import Timeline from '@mui/lab/Timeline';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import { GetServerSideProps } from 'next';
 
-import { TimelineCard, Title } from '@components';
+import { TimelineCard, Title, Footer } from '@components';
 import { LongTermTimeLineResponse, FooterResponse } from '@utils/types';
 import { fetchData } from 'lib/api';
 
@@ -11,19 +17,78 @@ type CombinedResponse = {
   footer: FooterResponse;
 };
 
-const MentorshipLongTermTimelinePage = ({ data }: CombinedResponse) => {
+const MentorshipLongTermTimelinePage = ({ data, footer }: CombinedResponse) => {
   return (
     <div>
       <Title title={'Long-Term Mentorship Timeline'} />
 
-      {data?.events?.items?.map((item, index) => (
-        <TimelineCard
-          key={index}
-          title={item.title}
-          description={item.description}
-          date={item.duration}
-        />
-      ))}
+      <Timeline
+        position="right"
+        sx={{
+          marginTop: '94px',
+        }}
+      >
+        {data?.events?.items?.map((item, index) => (
+          <TimelineItem
+            key={index}
+            sx={{
+              marginLeft: {
+                xs: '-50px',
+              },
+            }}
+          >
+            <TimelineSeparator>
+              <TimelineDot
+                color="primary"
+                sx={{
+                  margin: 0,
+                  width: {
+                    xs: '15px',
+                    sm: '24px',
+                    md: '32px',
+                  },
+                  height: {
+                    xs: '15px',
+                    sm: '24px',
+                    md: '32px',
+                  },
+                }}
+              />
+
+              {index + 1 !== data?.events?.items?.length && (
+                <TimelineConnector
+                  sx={{
+                    bgcolor: 'rgb(140,145,150)',
+                    width: {
+                      xs: '1.5px',
+                      sm: '2px',
+                      md: '2.5px',
+                    },
+                  }}
+                />
+              )}
+            </TimelineSeparator>
+            <TimelineContent
+              sx={{
+                flex: '10',
+                marginTop: '-30px',
+                marginLeft: {
+                  xs: '-10px',
+                  md: '-70px',
+                },
+              }}
+            >
+              <TimelineCard
+                title={item.title}
+                description={item.description}
+                date={item.duration}
+              />
+            </TimelineContent>
+          </TimelineItem>
+        ))}
+      </Timeline>
+
+      {footer && <Footer {...footer} />}
     </div>
   );
 };
