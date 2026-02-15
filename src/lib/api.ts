e41a13cdfef04aa45872daa5562aeac842e4cebd
Adni Sosa <144563583+AdniSosa@users.noreply.github.com>
2026-02-15 20:29:16 +0100

    Long-term timeline with card usage (#185)
    * long-term timeline with card usage
    
    * Added timeline lines and dots, footer, and reviewed the design
    
    * chore: sync pnpm lockfile
    
    * Added the breadcrumbs
    
    * fix: resolve pnpm merge conflicts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,6 +4,7 @@ import aboutUsPage from './responses/aboutUs.json';
 import aboutUsTeam from './responses/aboutUsTeam.json';
 import footerData from './responses/footer.json';
 import landingPageData from './responses/landingPage.json';
+import mentorShipLongTermTimeline from './responses/longTermTimeLine.json';
 import mentors from './responses/mentors.json';
 import mentorShipPage from './responses/mentorship.json';
 import mentorShipCodeofConduct from './responses/mentorshipCodeOfConduct.json';
@@ -27,6 +28,7 @@ const client = axios.create({
 const pageData = {
   landingPage: landingPageData,
   'mentorship/overview': mentorShipPage,
+  'mentorship/long-term-timeline': mentorShipLongTermTimeline,
   'programmes/study-groups': ourProgrammesPage,
   'about-us/celebrate-her': aboutUsPage,
   'mentorship/mentors': mentors,
